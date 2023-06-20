b5ee0780af434425ec1d193e9779643dbabfa84e
Daniel <dsalarcon3@soy.sena.edu.co>
2023-06-19 23:13:44 -0500

    Este commit se realiza por seguridad

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,11 +1,12 @@
 //Reglas de validación
 const number=/^[0-9]{5,10}$/
-const textName=/^[a-zA-ZñÑá-úÁ-Ú ]{3,50}$/
-const textApellido=/^[a-zA-ZñÑá-úÁ-Ú ]{3,50}$/
+const textName=/^[a-zA-ZñÑá-úÁ-Ú ]{3,}$/
+const textApellido=/^[a-zA-ZñÑá-úÁ-Ú ]{3,}$/
 const fechaLimite = new Date("01-01-2002")
 const mail=/(@misena.edu.co)/
+const pass=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$^&*?¿()_-]).{10,}$/
 
-let flag,flag2,flag3,flag4,flag5
+let flag,flag2,flag3,flag4,flag5,flag6,flag7
 
 //Acceder a los elementos que se van a estar validando
 const form=document.getElementById("form")
@@ -16,6 +17,9 @@ const nDocumento=form.nDocumento.value //Estoy accediendo por el valor
 const name=form.name.value
 const last_name=form.last_name.value
 const email=form.Email.value
+const password=form.password.value
+const confirmarPass=form.confirm_password.value
+
 
 
 
@@ -25,6 +29,8 @@ const f2=document.querySelector("#campoNombre .feedback")
 const f3=document.querySelector("#campoApellido .feedback")
 const f4=document.querySelector("#campoDeFecha .feedback")
 const f5=document.querySelector("#campoEmail .feedback")
+const f6=document.querySelector("#campoPassword .feedback")
+const f7=document.querySelector("#validarPass .feedback")
 
 
 
@@ -36,7 +42,7 @@ form.nDocumento.addEventListener("input",e=>{
         //Regla pasa la validación:
         form.nDocumento.setAttribute("class","success")
         f1.textContent="Solo puede digitar valores numéricos con una longitud mínimo de 5 caracter max 10"
-        f1.style.setProperty("visibility","hidden")
+        f1.style.setProperty("display","none")
         f1.style.setProperty("opacity","0")
         flag=true
     }else{
@@ -61,7 +67,7 @@ form.name.addEventListener("input",e=>{
     if(textName.test(e.target.value)){
         form.name.setAttribute("class","success")
         f2.textContent=`Solo se admiten caracteres alfanuméricos`
-        f2.style.setProperty("visibility","hidden")
+        f2.style.setProperty("display","none")
         f2.style.setProperty("opacity","0")
         flag2=true
     }else{
@@ -80,7 +86,7 @@ form.last_name.addEventListener("input",e=>{
     if (textApellido.test(e.target.value)) {
         form.last_name.setAttribute("class","success")
         f3.textContent='Solo se admiten caracteres alfanuméricos'
-        f3.style.setProperty("visibility","hidden")
+        f3.style.setProperty("display","none")
         f3.style.setProperty("opacity","0")
         flag3=true
     }else{
@@ -107,7 +113,7 @@ form.date_of_birth.addEventListener("change",e=>{
     if (parseIntoDate<fechaLimite) {
         form.date_of_birth.setAttribute("class","success")
         f4.textContent='Ojo para digitar su fecha de nacimiento, debe ser mayor de edad'
-        f4.style.setProperty("visibility","hidden")
+        f4.style.setProperty("display","none")
         f4.style.setProperty("opacity","0")
         flag4=true
     }else{
@@ -125,7 +131,7 @@ form.Email.addEventListener("input",e=>{
     if (mail.test(e.target.value)) {
         form.Email.setAttribute("class","success")
         f5.textContent='Solamente se permite el dominio @misena.edu.co'
-        f5.style.setProperty("visibility","hidden")
+        f5.style.setProperty("display","none")
         f5.style.setProperty("opacity","0")
         flag5=true
     }else{
@@ -137,16 +143,68 @@ form.Email.addEventListener("input",e=>{
     }
 })
 
+//PASSWORD
+form.password.addEventListener("input",e=>{
+    if (pass.test(e.target.value)) {
+        form.password.setAttribute("class","success")
+        f6.textContent=''
+        f6.style.setProperty("display","none")
+        f6.style.setProperty("opacity","0")
+        flag6=true
+    }else{
+        form.password.setAttribute("class","error")
+        f6.textContent='LA CONTRASEÑA NO CUMPLE CON LOS LINEAMIENTOS'
+        f6.style.setProperty("visibility","visible")
+        f6.style.setProperty("opacity","1")
+        flag6=false
+    }
+    })
 
 
 
+//VAL PASS
+form.confirm_password.addEventListener("input", (e) => {
+
+    const confirmPassword = form.confirm_password.value
+    const password=form.password.value
+
+    if (confirmPassword == password) {
+        form.confirm_password.setAttribute("class","success")
+        f7.style.setProperty("visibility","hidden")
+        f7.style.setProperty("opacity","0")
+        flag7=true
+    }else{
+        form.confirm_password.setAttribute("class","error")
+        f7.textContent='LA CONTRASEÑA NO CUMPLE CON LOS LINEAMIENTOS'
+        f7.style.setProperty("visibility","visible")
+        f7.style.setProperty("opacity","1")
+        flag7=false
+    }
+})
 
 
+//VENTANA MODAL
+let btnOpenModal=document.querySelector("#Terms-Conditions")
+
+let ventanaModal=document.querySelector(".TermsandConditions")
+
+let closeModal=document.querySelector(".closeM5")
+
+btnOpenModal.addEventListener("input",()=>{
+    ventanaModal.style.display="block"
+})
+
+closeModal.addEventListener("click",()=>{
+    ventanaModal.style.display="none"
+})
 
 
+//Aceptar los T&C
+let cerrarTerminos=document.querySelector("#Accept")
 
-
-
+cerrarTerminos.addEventListener("click",()=>{
+    ventanaModal.style.display="none"
+})
 
 
 
@@ -185,9 +243,15 @@ form.addEventListener("submit",e=>{
         alert('El dominio no es correcto')
         form.Email.focus()
         form.setAttribute("class","error")
-    }
-    
-    
+    }else if(form.password.value==null || form.password.value==0 || flag6==false){
+        alert('No funciuiona')
+        form.password.focus()
+        form.password.setAttribute("class","error")
+    }else if(form.confirm_password.value==null || form.confirmPassword.value==0 || flag7==false){
+        alert('Ojo con lo que ingresa!')
+        form.confirm_password.focus()
+        form.confirm_password.setAttribute("class","error")
+    }//else if(){}
     
     
     else{
